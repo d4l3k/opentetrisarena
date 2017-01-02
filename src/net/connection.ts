0e5455fbd3f9752c360bcfdb83bcf9cf16cd351c
Travis CI <rice@fn.lc>
2017-01-02 23:20:36 +0000

    Deploy to GitHub Pages: 3c0869f5e20fe3e14275ad0b97b17b8a1c9e98eb

--- src/net/connection.ts
+++
@@ -1,9 +0,0 @@
-import {Message} from './message';
-
-export interface Connection {
-  onMessage: (msg: Message, reliable: boolean, bytes: number) => void;
-  onOpen: () => void;
-  onClose: () => void;
-  send(msg: Message, reliable: boolean): void;
-  close(): void;
-}
